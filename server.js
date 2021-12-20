5b6330325e1bfce958349822f62a1e42a1bc57b7
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2021-12-20 14:17:59 +0100

    finished project

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/'});
+
 
 var app = express();
 
@@ -11,7 +14,15 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next)=> {
+  const {originalname, mimetype, size} = req.file;
 
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
